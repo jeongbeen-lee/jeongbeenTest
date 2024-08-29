c148c0a417f5da4931a5917fda6f1d9654a78aa6
jeongbeen-lee <silencio612@naver.com>
2024-08-29 19:51:31 +0900

    ㅎㅎ

---
+++ src/main/java/net/datasa/test1/IndexController.java
@@ -0,0 +1,13 @@
+package net.datasa.test1;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+	@GetMapping({"", "/"})
+	public String index() {
+		return "index";
+	}
+	
+
+}
